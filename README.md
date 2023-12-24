548b51fc6fb666edb81b470531818d77efd64525
Sulagna Pradhan <152047288+Sulagna-pradhan@users.noreply.github.com>
2023-12-24 21:03:13 +0530

    E-library webpage

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# library
+PlayLand E-Library:-
+
+Welcome to the PlayLand E-Library GitHub repository! This is the source code for a responsive e-library website that allows users to explore, discover, and download their favorite books.
+
+Features:-
+
+- **Responsive Design**: The website is designed to work seamlessly on various devices, providing a consistent user experience.
+- **Genres Dropdown**: Users can explore books by different genres through an easy-to-use dropdown menu.
+- **Editor's Choice**: Featured books that are popular and loved by readers.
+- **Top Authors**: Showcase of top authors with clickable links to their profiles.
+- **Trending Books**: A section highlighting trending books with links to each book.
+- **Books Series**: Explore famous book series with images and a "See All Series" button.
+- **About Us**: Learn more about PlayLand E-Library with a dedicated section.
+- **Footer Navigation**: Quick links to navigate to authors, trending books, and book series.
---
+++ script.js
@@ -0,0 +1,48 @@
+
+const header = document.querySelector("header");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+// Toggle mobile menu on hamburger button click
+hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+
+// Close mobile menu on close button click
+closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
