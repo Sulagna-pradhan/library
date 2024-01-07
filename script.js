dd3cefdacc2e2bb88620f6d96438a6764a4fd223
Sulagna Pradhan <152047288+Sulagna-pradhan@users.noreply.github.com>
2024-01-07 07:43:51 +0000

    new file:   .txt 	new file:   book_series.html 	deleted:    books-series.html 	new file:   footer.html 	modified:   index.html 	new file:   login/index.html 	new file:   login/style.css 	new file:   navbar.html 	new file:   new_york.html 	new file:   profile page/index.html 	new file:   profile page/upload.html 	modified:   script.js 	modified:   style.css 	new file:   trending_books.html

--- script.js
+++ script.js
@@ -1,41 +1,34 @@
-
 const header = document.querySelector("header");
 const hamburgerBtn = document.querySelector("#hamburger-btn");
 const closeMenuBtn = document.querySelector("#close-menu-btn");
 
 // Toggle mobile menu on hamburger button click
-hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+hamburgerBtn.addEventListener("click", () =>
+  header.classList.toggle("show-mobile-menu")
+);
 
 // Close mobile menu on close button click
 closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 
-
-
-
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
-
-
-
-
-
+};
 
 document.addEventListener("DOMContentLoaded", function () {
   // Get all author card links
@@ -52,7 +45,11 @@ document.addEventListener("DOMContentLoaded", function () {
       var authorName = link.querySelector("h3").textContent;
 
       // Redirect to the author's page with general overview
-      window.location.href = "author-overview.html?authorImage=" + encodeURIComponent(authorImage) + "&authorName=" + encodeURIComponent(authorName);
+      window.location.href =
+        "author-overview.html?authorImage=" +
+        encodeURIComponent(authorImage) +
+        "&authorName=" +
+        encodeURIComponent(authorName);
     });
   });
 
@@ -64,16 +61,23 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// This Varibale will store the top position
+var lastScrollTop;
+scrollnavbar = document.querySelector(".scrollNav"); // Get The NavBar
 
+window.addEventListener("scroll", function () {
+  var scrollTop = document.documentElement.scrollTop;
 
+  console.log(scrollTop - lastScrollTop);
 
+  if (scrollTop - lastScrollTop > 4 && scrollTop > lastScrollTop) {
+    this.setTimeout(function () {
+      scrollnavbar.style.top = "-81px";
+    }, 200);
+    //set the value to the negetive of height of navbar
+  } else if (scrollTop - lastScrollTop < -4 && scrollTop < lastScrollTop) {
+    scrollnavbar.style.top = "0";
+  }
 
-
-
-
-
-
-
-
-
-
+  lastScrollTop = scrollTop;
+});
